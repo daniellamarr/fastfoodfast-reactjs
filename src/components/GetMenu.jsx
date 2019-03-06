f1d9b11b2e1c42257a6d01fae60b46a8f6a62d5d
daniellamarr <daniel.anyaegbu@andela.com>
2019-03-06 12:36:48 +0100

    Delete addToCart utility file

--- src/components/GetMenu.jsx
+++ src/components/GetMenu.jsx
@@ -3,7 +3,6 @@ import React from 'react';
 import { connect } from 'react-redux';
 import PropTypes from 'prop-types';
 import { getAllMenu } from '../actions/Menu';
-import addToCart from '../utils/addToCart';
 
 /**
  * GetMenu
